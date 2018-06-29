2d49b73bc4405ffde250f0eee9f621aebea666e6
chandu <chandrasr.palle@gmail.com>
2018-06-30 01:01:37 +0200

    passport initialized

--- READEME.md
+++ READEME.md
@@ -4,3 +4,13 @@ When the last commit is not the initial commit
 No changes. i.e. Execute git reset HEAD~
 When the last commit is the initial commit
 Execute git update-ref -d HEAD followed by git rm -fr . git rm --cached -r .
+
+#Authentication demo Part-1
+*Setup folder structure
+*install packages
+*Add root route and template
+*Add secret route and template
+
+#Authentication demo Part-2
+*Create user model
+*Configure Passport
--- app.js
+++ app.js
@@ -1,9 +1,25 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
+const express               = require('express'),
+      bodyParser            = require('body-parser'),
+      mongoose              = require('mongoose'),
+      passport              = require('passport'),
+      localStrategy         = require('passport-local'),
+      passportLocalmongoose = require('passport-local-mongoose'),
+      User                  = require('./models/user');
 
+
+const app = express();
+mongoose.connect("mongodb://localhost/auth_demo");
 app.set("view engine", "ejs");
-// app.use(bodyParser)
+app.use(require('express-session')({
+    secret: "Rusty is the best and cutest dog",
+    resave: false,
+    saveUninitialized: false    
+}));
+app.use(passport.initialize());  //needed methods for passport to work
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res){
     res.render("home");
