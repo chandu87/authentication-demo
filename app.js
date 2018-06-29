444b5fda4f1a2fc48da536b5b873af1d53d38535
chandu <chandrasr.palle@gmail.com>
2018-06-29 16:30:12 +0200

    basic server created and home page served

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Authentiation");
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.set("view engine", "ejs");
+// app.use(bodyParser)
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.listen(3000, function(){
+    console.log("Server started at Port: 3000"); 
+});
