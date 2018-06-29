3f0a83df699281cfc031940de3d5fd8b166de420
chandu <chandrasr.palle@gmail.com>
2018-06-29 16:40:41 +0200

    secret page added

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ app.set("view engine", "ejs");
 app.get("/", function(req, res){
     res.render("home");
 });
+app.get("/secret", function(req, res){
+    res.render("secret");
+})
 
 app.listen(3000, function(){
     console.log("Server started at Port: 3000"); 
