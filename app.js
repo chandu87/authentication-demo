33377c944a0e1a72b26f409d18c78bfcd4f15502
chandu <chandrasr.palle@gmail.com>
2018-06-30 11:06:28 +0200

    sign up form added

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const express               = require('express'),
 const app = express();
 mongoose.connect("mongodb://localhost/auth_demo");
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(require('express-session')({
     secret: "Rusty is the best and cutest dog",
     resave: false,
@@ -21,12 +22,35 @@ app.use(passport.session());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+//============================== ROUTES =======================================
+
+
 app.get("/", function(req, res){
     res.render("home");
 });
 app.get("/secret", function(req, res){
     res.render("secret");
 })
+
+//Auth ROUTES
+
+//show Signup form
+app.get("/register", function(req, res){
+    res.render("register");
+});
+//handling user signup
+app.post("/register", function(req, res){
+    User.register(new User({username: req.body.username}),req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/secret");
+        });
+    });
+});
+
 
 app.listen(3000, function(){
     console.log("Server started at Port: 3000"); 
