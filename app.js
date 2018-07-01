25a0ca68a03105564fece5dfcd8275c1716cc361
chandu <chandrasr.palle@gmail.com>
2018-07-01 09:42:40 +0200

    logout and secret page authentication added

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ passport.deserializeUser(User.deserializeUser());
 app.get("/", function(req, res){
     res.render("home");
 });
-app.get("/secret", function(req, res){
+app.get("/secret", isLoggedIn, function(req, res){
     res.render("secret");
 })
 
@@ -67,7 +67,18 @@ app.post("/login",passport.authenticate("local", {
 
 });
 
+//logout
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/");
+});
 
+function isLoggedIn(req, res , next){
+    if(req.isAuthenticated()){
+       return next();
+    }
+    res.redirect("/login");
+}
 app.listen(3000, function(){
     console.log("Server started at Port: 3000"); 
 });
